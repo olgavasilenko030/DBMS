e5655f941df17367807cc2bebcdd9a0dd48253ac
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-02-08 23:37:20 +0500

    Teacher of Direction

---
+++ DML/SQLQuery3 -COUNT Teachers fo Directions.sql
@@ -0,0 +1,28 @@
+USE PV_319_Import;
+GO
+
+SELECT 
+		[Направление обучения] = direction_name,
+		[Количество преподавателей] = 
+							(SELECT SUM(teachers.number_of_teachers)
+							FROM (SELECT discipline_id, COUNT(teacher_id) AS number_of_teachers
+							FROM Teachers,Disciplines,TeachersDisciplinesRelation
+							WHERE	teacher = teacher_id
+							AND		discipline =discipline_id
+							GROUP BY discipline_id
+							)teachers,TeachersDisciplinesRelation
+
+							WHERE	discipline = discipline_id AND direction = direction_id
+							)
+
+FROM Directions,Disciplines,DisciplinesDirectionsRelation
+WHERE	direction =direction_id
+AND		discipline =discipline_id
+--GROUP BY direction_name
+;
+
+SELECT discipline_name,COUNT(teacher_id) AS number_of_teachers
+					FROM Teachers,Disciplines,TeachersDisciplinesRelation
+					WHERE teacher =teacher_id
+					AND discipline =discipline_id
+					GROUP BY discipline_name
--- DML/dz - LESSON.sql
+++
@@ -1,7 +0,0 @@
-USE PV_319_DDL;
-GO
-
-SELECT
-			
-			COUNT(time)*COUNT(lesson_id)	AS N'Количество часов'
-FROM		Schedule;
