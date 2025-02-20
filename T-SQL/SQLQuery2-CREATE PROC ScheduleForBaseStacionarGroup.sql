a66067a4995ea953a9b63ae9cd4b732a1d4881fc
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-02-21 00:04:44 +0500

    Alternating DONE

---
+++ T-SQL/SQLQuery2-CREATE PROC ScheduleForBaseStacionarGroup.sql
@@ -0,0 +1,57 @@
+USE PV_319_Import;
+SET DATEFIRST 1;
+GO
+
+ALTER PROCEDURE sp_ScheduleForBaseStacionarGroup
+	@group_name				NVARCHAR(16),
+	@discipline_name		NVARCHAR(150),
+	@teacher_last_name		NVARCHAR(50),
+	@start_date				DATE,
+	@time					TIME(0),
+	@learning1				TINYINT,
+	@learning2				TINYINT,
+	@learning3				TINYINT,
+	@alternating_day		TINYINT,
+	@first_week_present		BIT
+
+AS
+BEGIN
+	DECLARE	@group					AS INT			=	(SELECT group_id		FROM Groups			WHERE group_name=@group_name);
+	DECLARE	@discipline				AS SMALLINT	=	(SELECT discipline_id	FROM Disciplines	WHERE discipline_name LIKE @discipline_name);
+	DECLARE @teacher				AS SMALLINT	=	(SELECT teacher_id		FROM Teachers		WHERE last_name =@teacher_last_name);
+	DECLARE @date					AS DATE		=	@start_date;
+	DECLARE @current_week_present	AS BIT		=	@first_week_present;
+	DECLARE @number_of_lessons		AS TINYINT		=	(SELECT number_of_lessons FROM Disciplines	WHERE discipline_id=@discipline);
+	DECLARE @lesson_number			AS TINYINT	=	0;
+	
+
+	WHILE (@lesson_number<@number_of_lessons)
+	BEGIN
+	PRINT(@date);
+	PRINT(DATENAME(WEEKDAY,@date));
+	PRINT(DATEPART(WEEKDAY,@date));
+	PRINT(@lesson_number+1);
+	PRINT(@time);
+	SET @lesson_number=@lesson_number+1;
+	PRINT(@lesson_number+1);
+	PRINT(DATEADD(MINUTE,95,@time));
+	SET @lesson_number=@lesson_number+1;
+	PRINT('---------------------------------');
+
+	PRINT(DATENAME(WEEKDAY,@date));
+	PRINT(@alternating_day);
+
+		IF(DATEPART(WEEKDAY,@date)=@alternating_day)
+		BEGIN
+			SET @date = DATEADD(DAY,2,@date);
+		END
+		ELSE IF(DATEPART(WEEKDAY,@date)=4)
+		BEGIN
+			SET @date = IIF(@current_week_present=1,DATEADD(DAY,7,@date),DATEADD(DAY,5,@date))
+		END
+		PRINT('Current_week_present')
+		PRINT(@current_week_present);
+		SET @current_week_present= IIF(@current_week_present=1,0,1);
+		PRINT('===============================');
+	END
+END
--- T-SQL/SQLQuery2-Schedule procedures test.sql
+++ T-SQL/SQLQuery2-Schedule procedures test.sql
@@ -2,13 +2,19 @@ USE PV_319_Import;
 GO
 
 --EXEC sp_SetSheduleForStacionarGroup N'PV_319',N'%UML%',N'Ковтун', N'2024-08-01', N'18:30';
-EXEC sp_SetSheduleForStacionarGroup N'PD_321',N'%MS SQL Server%',N'Покидюк', N'2024-06-01', N'13:30';
+--EXEC sp_SetSheduleForStacionarGroup N'PD_321',N'%MS SQL Server%',N'Покидюк', N'2024-06-01', N'13:30';
 
-EXEC sp_PrintScheduleForGroup N'PD_321';
+--EXEC sp_PrintScheduleForGroup N'PD_321';
 
 --EXEC sp_PrintScheduleForGroupPeriod N'PV_319','2024-06-01','2024-07-01';
 
-DECLARE @salary SMALLMONEY;
-EXEC @salary= sp_CountTeachersMonthSalary N'Ковтун',2024,06;
-PRINT (@salary)
+--DECLARE @salary SMALLMONEY;
+--EXEC @salary= sp_CountTeachersMonthSalary N'Ковтун',2024,06;
+--PRINT (@salary)
 --SELECT *FROM Schedule;
+ 
+
+PRINT(DATENAME(WEEKDAY,N'2023-11-23'));
+PRINT(DATEPART(WEEKDAY,N'2023-11-23'));
+
+EXEC sp_ScheduleForBaseStacionarGroup N'PV_319', N'Процедурное программирование %', N'Ковтун', N'2023-11-23', '18:30', 2,4,6,2,1;
