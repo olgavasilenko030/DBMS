dacf64516350e3018869b3ed9a3eabc5feb97453
Vasil_th18@top-academy.ru <Vasil_th18@top-academy.ru>
2025-03-06 21:16:58 +0500

    GetEasterDate DONE

--- Functions/SQLQuery0- Check functions.sql
+++ Functions/SQLQuery0- Check functions.sql
@@ -2,28 +2,30 @@ USE PV_319_Import;
 SET DATEFIRST 1;
 GO
 
-PRINT (dbo.GetGroupID(N'PV_319'));
-PRINT(dbo.GetDisciplineID(N'Hardware%'));
---EXEC sp_PrintScheduleForGroup N'PV_319';
-PRINT(dbo.CountSpentLessons(N'PV_319', N'Процедурное программирование%'));
-PRINT (dbo.GetNewYearHolidaysStartDate(2024));
-PRINT(dbo.GetSummerHolidaysStart(2023));
-PRINT(dbo.GetHolidaysDuration(N'Летние%'));
+--PRINT (dbo.GetGroupID(N'PV_319'));
+--PRINT(dbo.GetDisciplineID(N'Hardware%'));
+----EXEC sp_PrintScheduleForGroup N'PV_319';
+--PRINT(dbo.CountSpentLessons(N'PV_319', N'Процедурное программирование%'));
+--PRINT (dbo.GetNewYearHolidaysStartDate(2024));
+--PRINT(dbo.GetSummerHolidaysStart(2023));
+--PRINT(dbo.GetHolidaysDuration(N'Летние%'));
 
-PRINT('--------------------------------------');
+--PRINT('--------------------------------------');
 
-DECLARE @holiday	AS NVARCHAR(150)	= N'Лет%';
-DECLARE @year		AS SMALLINT			=2025;
-PRINT (dbo.GetHolidaysStartDate(@holiday,@year));
+--DECLARE @holiday	AS NVARCHAR(150)	= N'Лет%';
+--DECLARE @year		AS SMALLINT			=2025;
+--PRINT (dbo.GetHolidaysStartDate(@holiday,@year));
 
-DECLARE		@month		AS TINYINT =(SELECT [month]FROM Holidays WHERE holiday_name LIKE @holiday);
-		DECLARE		@day		AS TINYINT =(SELECT [day]FROM Holidays WHERE holiday_name LIKE @holiday);
-		DECLARE		@start_date	AS DATE = 
-		(
-		CASE			
-		WHEN		@holiday		LIKE N'Нов%' THEN dbo.GetNewYearHolidaysStartDate(@year) 
-		WHEN		@holiday		LIKE N'Лет%'		THEN dbo.GetSummerHolidaysStart(@year) 
-		WHEN		@month != 0 AND @day !=0 THEN DATEFROMPARTS(@year,@month,@day)
-		END
-		)
-		PRINT(@start_date);
+--DECLARE		@month		AS TINYINT =(SELECT [month]FROM Holidays WHERE holiday_name LIKE @holiday);
+--		DECLARE		@day		AS TINYINT =(SELECT [day]FROM Holidays WHERE holiday_name LIKE @holiday);
+--		DECLARE		@start_date	AS DATE = 
+--		(
+--		CASE			
+--		WHEN		@holiday		LIKE N'Нов%' THEN dbo.GetNewYearHolidaysStartDate(@year) 
+--		WHEN		@holiday		LIKE N'Лет%'		THEN dbo.GetSummerHolidaysStart(@year) 
+--		WHEN		@month != 0 AND @day !=0 THEN DATEFROMPARTS(@year,@month,@day)
+--		END
+--		)
+--		PRINT(@start_date);
+
+PRINT (dbo.GetEasterDate(2005));
